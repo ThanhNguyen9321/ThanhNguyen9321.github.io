d4607d68a74dd188134707e9f31bdee826d8b12a
ThanhNguyen9321 <138231444+ThanhNguyen9321@users.noreply.github.com>
2023-07-22 22:05:11 +0700

    22/7

--- js/script.js
+++ js/script.js
@@ -1,20 +1,48 @@
+const EMAIL_REGEX =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function validateEmail(value) {
+  return value.match(EMAIL_REGEX);
+}
+
+function displayInfo(isShow) {
+  // true - show, false - hide
+  const infoElement = document.querySelector("#about-info .about-info");
+  if (infoElement) {
+    if (isShow) {
+      infoElement.style.display = "block";
+    } else {
+      infoElement.style.display = "none";
+    }
+  }
+}
+
+function displaySubmitButton(isShow) {
+  // true - show, false - hide
+  const submitButton = document.querySelector(".submit-email");
+  if (submitButton) {
+    if (isShow) {
+      submitButton.style.display = "block";
+    } else {
+      submitButton.style.display = "none";
+    }
+  }
+}
+
 function handleSubmit() {
   //lấy giá trị của nhập vào email
   const emailValue = document.getElementById("email").value;
+
   //lấy hiển thị lỗi email
   const emailError = document.getElementById("email-error");
+
   // điều kiện để email đúng
-  const regex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const checkMail = validateEmail(emailValue);
 
-  const checkMail = emailValue.match(regex);
-  //lấy phần chứa thông tin user
-  const sectionContent = document.querySelector("#about-info .about-info");
-
-  const submitControl = document.querySelector(".submit-email");
   if (checkMail) {
-    sectionContent.style.display = "block";
-    submitControl.style.display = "none";
+    //lấy phần chứa thông tin user
+    displayInfo(true);
+    displaySubmitButton(false);
     emailError.innerHTML = "";
   } else {
     emailError.innerHTML = "email không đúng, vui lòng nhập lại";
